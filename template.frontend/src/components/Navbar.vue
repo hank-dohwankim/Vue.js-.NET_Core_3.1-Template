04e25b47dea9b99cd737c69f9d0f138611b6f17e
hank.dohwankim <hank.dohwankim@gmail.com>
2020-06-11 22:58:41 -0500

    Navbar content position change

--- template.frontend/src/components/Navbar.vue
+++ template.frontend/src/components/Navbar.vue
@@ -1,25 +1,12 @@
 <template>
   <nav class="nav-extended">
     <div class="nav-wrapper">
-      <ul>
-        <li>
+      <div class="brand-logo center">
+        <ul id="nav-mobile" class="right hid-on-med-and-down">
           <router-link :to="{name: 'Index'}">
             <!-- <router-link path="/"> -->
             <div>Home</div>
           </router-link>
-        </li>
-      </ul>
-      <div class="brand-logo center">
-        <ul id="nav-mobile" class="right hid-on-med-and-down">
-          <li>
-            <a href="#">Post</a>
-          </li>
-          <li>
-            <a href="#">Developer</a>
-          </li>
-          <li>
-            <a href="#">Category</a>
-          </li>
         </ul>
       </div>
       <ul id="nav-mobile" class="right hid-on-med-and-down">
